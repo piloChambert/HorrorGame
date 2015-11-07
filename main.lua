808512c499ff072cdd5e4387214422315949c6fb
piloChambert <pierrelaurent.chambert@gmail.com>
2015-11-07 11:52:22 +0100

    added new sounds

--- main.lua
+++ main.lua
@@ -1,42 +1,64 @@
+StaticSound = {}
 
+function StaticSound.new(filename, x, y, z, ref, max)
+	local self = setmetatable({}, StaticSound)
+
+	self.position = {x = x, y = y, z = z}
+	self.source = love.audio.newSource(filename, "static")
+	self.source:setPosition(x, y, z)
+	self.source:setAttenuationDistances(ref, max)
+	self.source:setLooping(true)
+	self.source:play()
+
+	return self
+end
+
+setmetatable(StaticSound, { __call = function(_, ...) return StaticSound.new(...) end })
 
 gameState = {
 	playerPosition = {x = 0, y = 0, z = 0},
 	playerAngular = {x = 0, y = 0, z = 0}, -- angle
+	footstepSound = nil,
 
-	testSound = nil
+	sounds = {}
 }
 
 function gameState:load()
-	self.testSound = { pos = {x = 40, y = 0, z = 0}}
+	table.insert(self.sounds, StaticSound("water_fall.wav", 40, 0, 0, 10, 10))
+	table.insert(self.sounds, StaticSound("engine.wav", 80, 0, 20, 5, 10))
+	table.insert(self.sounds, StaticSound("siren.wav", 0, 0, 50, 5, 10))
+	table.insert(self.sounds, StaticSound("water_fall.wav", 90, 0, 100, 10, 10))
 
-	self.testSound.source = love.audio.newSource("water_fall.wav", "static")
-	self.testSound.source:setPosition(self.testSound.pos.x, self.testSound.pos.y, self.testSound.pos.z)
-	self.testSound.source:setAttenuationDistances(10, 10)
-	self.testSound.source:setLooping(true)
-	self.testSound.source:play()
-
+	self.footstepSound = StaticSound("footsteps_wood.wav", 0, 0, 0, 0, 0)
+	self.footstepSound.source:setVolume(0)
 end
 
 function gameState:update(dt)
 	local playerOrientation = {x = math.cos(self.playerAngular.y), y = 0, z = math.sin(self.playerAngular.y)}
 	local playerSideVector = {x = playerOrientation.z, y = 0, z = -playerOrientation.x}
 
+	local footstepVolume = 0
 	if love.keyboard.isDown("z") then
 	   	self.playerPosition.x = self.playerPosition.x + playerOrientation.x * 8.0 * dt
    		self.playerPosition.z = self.playerPosition.z + playerOrientation.z * 8.0 * dt 	
+   		footstepVolume = 1
 	elseif love.keyboard.isDown("s") then
 	   	self.playerPosition.x = self.playerPosition.x - playerOrientation.x * 8.0 * dt
    		self.playerPosition.z = self.playerPosition.z - playerOrientation.z * 8.0 * dt 			
+   		footstepVolume = 1
 	end
 
 	if love.keyboard.isDown("q") then
 	   	self.playerPosition.x = self.playerPosition.x + playerSideVector.x * 8.0 * dt
    		self.playerPosition.z = self.playerPosition.z + playerSideVector.z * 8.0 * dt 	
+  		footstepVolume = 1
 	elseif love.keyboard.isDown("d") then
 	   	self.playerPosition.x = self.playerPosition.x - playerSideVector.x * 8.0 * dt
    		self.playerPosition.z = self.playerPosition.z - playerSideVector.z * 8.0 * dt 			
+  		footstepVolume = 1
 	end
+
+	self.footstepSound.source:setVolume(footstepVolume)
 
 
 	love.audio.setPosition(self.playerPosition.x, self.playerPosition.y, self.playerPosition.z)
@@ -69,7 +91,9 @@ function gameState:draw()
 
 	love.graphics.line(self.playerPosition.x, self.playerPosition.z, self.playerPosition.x + playerOrientation.x * 15, self.playerPosition.z + playerOrientation.z * 15)
 
-	love.graphics.point(self.testSound.pos.x, self.testSound.pos.z)
+	for k, v in pairs(self.sounds) do
+		love.graphics.point(v.position.x, v.position.z)
+	end
 
 	love.graphics.pop()
 end
