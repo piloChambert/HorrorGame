e20048f4bf224c2790e698b97205448c5e54470b
PLCGame <pierrelaurent.chambert@gmail.com>
2015-11-08 19:01:48 +0100

    Added 3d rendering

--- main.lua
+++ main.lua
@@ -23,9 +23,25 @@ gameState = {
 	sounds = {}
 }
 
-mouseSensibility = 0.01
+mouseSensibility = 0.005
 indicatorVisibleTime = 0.1
 indicatorFadeTime = 0.1
+
+levelWalls = {  {x = -50, y = 0, z = -50},
+			 	{x = -40, y = 0, z = -50},
+			 	{x = -30, y = 0, z = -50},
+			 	{x = -20, y = 0, z = -50},
+			 	{x = -10, y = 0, z = -50},
+			 	{x = 0, y = 0, z = -50},
+			 	{x = 10, y = 0, z = -50},
+			 	{x = 20, y = 0, z = -50},
+			 	{x = 30, y = 0, z = -50},
+			 	{x = 40, y = 0, z = -50},
+			 	{x = 50, y = 0, z = -50},
+			 	{x = 50, y = 0, z = 50},
+			 	{x = -50, y = 0, z = 50},
+			 	{x = -50, y = 0, z = -50}
+			 }
 
 function gameState:load()
 	table.insert(self.sounds, StaticSound("water_fall.wav", 40, 0, 0, 10, 10))
@@ -100,7 +116,8 @@ function gameState:mousemoved(x, y, dx, dy)
 	--print(dx, self.playerAngular.y)
 end
 
-function gameState:projectPoint(p)
+-- transform a point in eye space
+function gameState:transformPoint(p)
 	local tx = p.x - self.playerPosition.x
 	local ty = p.y - self.playerPosition.y
 	local tz = p.z - self.playerPosition.z
@@ -113,27 +130,125 @@ function gameState:projectPoint(p)
 	local y = ty
 	local z = rx.z * tx + rz.z * tz
 
-	return x, y, z
+	return {x = x, y = y, z = z}
+end
+
+function projectPoint(p, zoom)
+	local projectedPoint = {}
+
+	projectedPoint.x = p.x / -p.z * zoom
+	projectedPoint.y = p.y / -p.z * zoom	
+	projectedPoint.z = -p.z -- distance to camera
+
+	return projectedPoint
 end
 
 function gameState:draw()
 	love.graphics.setColor(255, 255, 255, 255)
+	local winW, winH = love.window.getDimensions()
 
-	-- debug draw
+	-- map debug draw
 	if true then
 		love.graphics.push()
-		love.graphics.translate(256, 256)
+		love.graphics.translate(winW / 2, winH / 2)
 
-		local px, py, pz = self:projectPoint(self.playerPosition)
-		love.graphics.point(px, pz)
-		love.graphics.line(px, pz, px, pz - 15)
+		-- player
+		local playerPos = self:transformPoint(self.playerPosition)
+		love.graphics.point(playerPos.x, playerPos.z)
+		love.graphics.line(playerPos.x, playerPos.z, playerPos.x, playerPos.z - 15)
 
+		-- objects
 		for k, v in pairs(self.sounds) do
-			x, y, z = self:projectPoint(v.position)
-			love.graphics.point(x, z)
+			local p = self:transformPoint(v.position)
+			love.graphics.point(p.x, p.z)
 		end
+
+		-- walls
+		local prevPoint = nil
+		for k, v in pairs(levelWalls) do
+			local p = self:transformPoint(v)
+
+			if not (prevPoint == nil) then
+				love.graphics.line(prevPoint.x, prevPoint.z, p.x, p.z)
+			end
+
+			prevPoint = p
+		end
+
+
 		love.graphics.pop()
 	end
+
+	-- draw walls
+	love.graphics.push()
+	love.graphics.translate(winW / 2, winH / 2)
+
+	local prevPoint = nil
+	for k, v in pairs(levelWalls) do
+		-- y = 0, because it's just 2D point on x/z plane
+		local currentPoint = self:transformPoint(v)
+
+		local p1 = nil
+		local p2 = nil
+
+		--love.graphics.line(x, y1, x, y2)
+		if not (prevPoint == nil)  then
+			if currentPoint.z < 0 and prevPoint.z < 0 then
+				p1 = currentPoint
+				p2 = prevPoint
+
+				--love.graphics.setColor(255, 255, 255, 255)
+			end
+
+			if currentPoint.z < 0 and prevPoint.z > 0 then
+				local l =  (-currentPoint.z * 0.999) / (-currentPoint.z + prevPoint.z)
+				local clampedPoint = {}
+				clampedPoint.x = l * (prevPoint.x - currentPoint.x) + currentPoint.x
+				clampedPoint.z = l * (prevPoint.z - currentPoint.z) + currentPoint.z
+
+				p1 = currentPoint
+				p2 = clampedPoint
+
+				--love.graphics.setColor(255, 0, 0, 255)
+			end
+
+			if currentPoint.z > 0 and prevPoint.z < 0 then
+				local l =  (-prevPoint.z * 0.999) / (-prevPoint.z + currentPoint.z)
+				local clampedPoint = {}
+
+				clampedPoint.x = l * (currentPoint.x - prevPoint.x) + prevPoint.x
+				clampedPoint.z = l * (currentPoint.z - prevPoint.z) + prevPoint.z
+
+				p1 = clampedPoint
+				p2 = prevPoint
+
+				--love.graphics.setColor(0, 0, 255, 255)
+			end
+
+			-- draw if visible
+			if not (currentPoint.z >= 0 and prevPoint.z >= 0) then
+				-- project points
+				local A = projectPoint({x = p1.x, y = -5, z = p1.z}, winW / 2)
+				local B = projectPoint({x = p1.x, y = 5, z = p1.z}, winW / 2)
+				local C = projectPoint({x = p2.x, y = -5, z = p2.z}, winW / 2)
+				local D = projectPoint({x = p2.x, y = 5, z = p2.z}, winW / 2)
+
+				--love.graphics.polygon('line', A.x, A.y, C.x, C.y, D.x, D.y, B.x, B.y)
+
+				local wallInvVisibility = 26
+				local mesh = love.graphics.newMesh({ { A.x, A.y, 0, 0, 255, 255, 255, math.max(255 - A.z * wallInvVisibility, 0)},
+													 { C.x, C.y, 1, 0, 255, 255, 255, math.max(255 - C.z * wallInvVisibility, 0)},
+													 { D.x, D.y, 1, 1, 255, 255, 255, math.max(255 - D.z * wallInvVisibility, 0)},
+													 { B.x, B.y, 0, 1, 255, 255, 255, math.max(255 - B.z * wallInvVisibility, 0)}}, nil)
+				love.graphics.draw(mesh)
+			end
+		end
+		--end
+
+		prevPoint = currentPoint
+	end
+	love.graphics.pop()
+
 
 	-- draw rotation indicator
 	local indicatorAlpha = 255
@@ -142,7 +257,6 @@ function gameState:draw()
 	end
 
 	love.graphics.setColor(255, 255, 255, indicatorAlpha)
-	winW, winH = love.window.getDimensions()
 	love.graphics.draw(self.indicator, 96, winH - 96, self.indicatorRotation, 1, 1, 64, 64)
 
 end
